adeb6567fc985393626251fac108e339183ac662
whispermind <h4vefun.dieyoung@gmail.com>
2021-09-09 20:35:14 +0300

    branch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import image from './bg1.jpg'
+import img from './bg1.jpg'
 import './App.css';
 import './index.css'
 import Header from './components/header/index.js'
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <Header title='the first' descr='smtng i should say' />
-      <Layout title='heading 1' descr='some description' urlBg={image} />
+      <Layout title='heading 1' descr='some description' urlBg={img} />
       <Layout title='heading 2' descr='some description' colorBg="#e2e2e2" />
-      <Layout title='heading 3' descr='some description' urlBg={image} />
+      <Layout title='heading 3' descr='some description' urlBg={img} />
       <Footer />
     </div>
   );
